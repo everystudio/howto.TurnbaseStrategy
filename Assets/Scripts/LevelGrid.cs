b3d329657d50bb69d2d1f70c82331063388ec422
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-03 00:45:34 +0900

    Level Grid チャレンジ

---
+++ Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGrid : MonoBehaviour
+{
+    [SerializeField] private Transform gridDebugObjectPrefab;
+    private GridSystem gridSystem;
+    private void Awake()
+    {
+        gridSystem = new GridSystem(10, 10, 2f);
+
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+
+    }
+
+    // Challange
+    // 下３つのメソッドを完成させてみよう
+    // GridObjectにユニットをセットしたりゲットしたりするメソッドを追加しましょう
+    // GridObject.ToStringを変更して、Unitがいるかどうかを確認できるようにしましょう
+    public void SetUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+    }
+    public Unit GetUnitAtGridPosition(GridPosition gridPosition)
+    {
+        return null;
+    }
+    public void ClearUnitAtGridPosition(GridPosition gridPosition)
+    {
+    }
+
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    [SerializeField] private Transform gridDebugObjectPrefab;
-    private GridSystem gridSystem;
-    void Start()
+    private void Start()
     {
-        gridSystem = new GridSystem(10, 10, 2f);
-
-        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
-
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetMousePosition()));
-
     }
 }
