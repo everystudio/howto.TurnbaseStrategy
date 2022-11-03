9cc6506527f31beea91dd7655b10cee555890416
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-03 17:27:16 +0900

    Camera Zoom ベタかきしたところ

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,18 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtual;
+
+    CinemachineTransposer cinemachineTransposer;
+    private Vector3 targetFollowOffset;
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtual.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
     private void Update()
     {
         Vector3 inputMoveDir = Vector3.zero;
@@ -41,7 +53,19 @@ public class CameraController : MonoBehaviour
         float rotationSpeed = 100f;
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
 
-
-
+        CinemachineTransposer cinemachineTransposer = cinemachineVirtual.GetCinemachineComponent<CinemachineTransposer>();
+        float zoomAmount = 1f;
+        if (0 < Input.mouseScrollDelta.y)
+        {
+            targetFollowOffset.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffset.y += zoomAmount;
+        }
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        float zoomSpeed = 5f;
+        cinemachineTransposer.m_FollowOffset =
+            Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
     }
 }
