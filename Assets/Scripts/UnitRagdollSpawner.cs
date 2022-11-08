dc3e13cb3aac53484ddc1d8f90cb8663257e4350
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-09 01:08:42 +0900

    倒したらラグドールとすり替わる処理

---
+++ Assets/Scripts/UnitRagdollSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitRagdollSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollPrefab;
+
+    private Healthsystem healthsystem;
+
+    private void Awake()
+    {
+        healthsystem = GetComponent<Healthsystem>();
+        healthsystem.OnDead += Healthsystem_OnDead;
+    }
+
+    private void Healthsystem_OnDead(object sender, System.EventArgs e)
+    {
+        Instantiate(ragdollPrefab, transform.position, transform.rotation);
+    }
+}
