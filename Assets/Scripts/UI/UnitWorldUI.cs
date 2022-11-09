5091dca9f519ba0acb0f46cba3f29cdd558c9a82
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-09 23:45:56 +0900

    Unit World UI とりあえずアクションポイントが表示更新される

---
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UnitWorldUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI actionPointsText;
+    [SerializeField] private Unit unit;
+
+    private void Start()
+    {
+        Unit.OnAnyActionPointsChanged += (sender, e) =>
+        {
+            UpdateActionPointsText();
+        };
+        UpdateActionPointsText();
+    }
+
+    private void UpdateActionPointsText()
+    {
+        actionPointsText.text = unit.GetActionPoints().ToString();
+    }
+}
