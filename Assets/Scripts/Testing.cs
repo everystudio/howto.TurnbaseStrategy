b55c847732297f3f5c13a8044705150b6784be8e
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-17 01:22:19 +0900

    LevelDesign

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -16,12 +16,12 @@ public class Testing : MonoBehaviour
             GridPosition startGridPosition = new GridPosition(0, 0);
 
             List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPosition, mouseGridPosition);
-
+            Debug.Log(gridPositionList.Count);
             for (int i = 0; i < gridPositionList.Count - 1; i++)
             {
                 Debug.DrawLine(
-                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
-                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]) + Vector3.up * 0.02f,
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]) + Vector3.up * 0.02f,
                     Color.white, 10f
                     );
             }
