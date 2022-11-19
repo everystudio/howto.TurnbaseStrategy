bb2ce6a9411c5230958a1bda7000c28649add325
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-19 10:23:08 +0900

    Grenade Action グレネード投げてダメージが入るところまで

---
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeProjectile : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+        float moveSpeed = 15f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float reachTargetDistance = .2f;
+        if (Vector3.Distance(transform.position, targetPosition) < reachTargetDistance)
+        {
+            float damageRadius = 4f;
+            Collider[] colliderArray = Physics.OverlapSphere(targetPosition, damageRadius);
+
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent<Unit>(out Unit targetUnit))
+                {
+                    targetUnit.Damage(30);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void Setup(GridPosition targetGridPosition)
+    {
+        targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+    }
+
+}
