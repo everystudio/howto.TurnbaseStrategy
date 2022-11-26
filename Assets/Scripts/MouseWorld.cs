5b457c780fb7a9cf540792935e58fb3af9eef607
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-26 21:33:07 +0900

    ファイルの場所変更など コマンドの処理を変更しようとし始めている

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -1,23 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MouseWorld : MonoBehaviour
 {
     public static MouseWorld Instance;
+    public static event EventHandler<GridPosition> OnCurrentGridChanged;
+
+    public GridPosition CurrentGrid => currentGrid;
+    private GridPosition currentGrid;
 
     [SerializeField] private LayerMask groundLayerMask;
 
     private void Awake()
     {
         Instance = this;
+        currentGrid = new GridPosition(0, 0);
     }
-    /*
+
     private void Update()
     {
-        transform.position = GetMousePosition();
+        transform.position = GetPosition();
+        GridPosition gridPosition = LevelGrid.Instance.GetGridPosition(GetPosition());
+        if (currentGrid != gridPosition)
+        {
+            currentGrid = gridPosition;
+            OnCurrentGridChanged?.Invoke(this, gridPosition);
+        }
     }
-    */
+
     public static Vector3 GetPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
---
+++ Assets/Scripts/Utils/StateMachineBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class StateMachineBase<T> : MonoBehaviour where T : StateMachineBase<T>
+{
+    private StateBase<T> currentState;
+    [SerializeField] private string debugStateName;
+
+    public void ChangeState(StateBase<T> nextState)
+    {
+        if (currentState != null)
+        {
+            currentState.OnExitState();
+        }
+        currentState = nextState;
+        currentState.OnEnterState();
+        debugStateName = currentState.ToString();
+    }
+
+    private void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.OnUpdate();
+        }
+    }
+}
